1eb8d426527d4815622258e33509516c85c90fe6
Gisele <49593120+iandroPaz@users.noreply.github.com>
2022-11-23 03:11:04 -0300

    fix: logical errors and attribution error

--- src/core/schemas/checkMovie.ts
+++ src/core/schemas/checkMovie.ts
@@ -1,13 +1,21 @@
 import { IMovie } from '../utils/types';
 
-export async function checkMovie(movie: any):Promise<IMovie> {
-  let response:IMovie;
+const response:IMovie = {
+  id: '0', 
+  title: '', 
+  original_title: '',
+  description: '',
+  release_date: null,
+  rt_score: null
+}
+
+export async function checkMovie(movie: IMovie):Promise<IMovie> {
   response.id = (movie.id !== null && movie.id !== '') && movie.id;
   response.title = (movie.title !== null && movie.title !== '') && movie.title;
   response.original_title = (movie.original_title !== null && movie.original_title !== '') && movie.original_title;
   response.description = (movie.description !== null && movie.description !== '') && movie.description;
-  response.release_date = (movie.release_date !== null && movie.release_date !== '') && movie.release_date;
-  response.rt_score = (movie.rt_score !== null && movie.rt_score !== '') && movie.rt_score;
+  response.release_date = (movie.release_date !== null) && movie.release_date;
+  response.rt_score = (movie.rt_score !== null) && movie.rt_score;
   
   return instanceOfMovie(response) && response;
 } 
