ac3d574c58be82f13bbc6e492613e26d4ac71ced
Gisele <49593120+iandroPaz@users.noreply.github.com>
2022-11-23 19:05:50 -0300

    Feat: Create-new-service
    Service of findAll and pagination

--- src/core/services/movie.ts
+++ src/core/services/movie.ts
@@ -1,11 +1,20 @@
-import { Op } from "sequelize";
 import MovieRepository from "../db/repositories/movie"
 import { IMovie } from "../utils/types"
 
 export const createMovie = async (movie:IMovie) =>{
-    return MovieRepository.create(movie, null);
+    return MovieRepository.create(movie);
 }
 
-export const findOneMovieById =async (movieId:String) => {
+export const findOneMovieById = async (movieId:String) => {
     return MovieRepository.selectOne({ where: { id: movieId } })
+}
+
+export const findAllWithPagination =async (limitReq:Number, offsetReq:Number) => {
+    if (limitReq !== 0 && offsetReq !== 0){
+        return MovieRepository.selectWithPagination({ limit: limitReq, offset: offsetReq, order: [['release_date', 'DESC']] });
+    }
+    if (limitReq !== 0){
+        return MovieRepository.selectWithPagination({ limit: limitReq, order: [['release_date', 'DESC']] });
+    }
+    return MovieRepository.selectWithPagination({ order: [['release_date', 'DESC']] });
 }
