9d4fb227fc7d318a6a69f6d17255d18b091d0341
Gisele <49593120+iandroPaz@users.noreply.github.com>
2022-11-23 03:11:38 -0300

    feat: create new route to consulting DB

--- src/core/services/movie.ts
+++ src/core/services/movie.ts
@@ -1,6 +1,11 @@
+import { Op } from "sequelize";
 import MovieRepository from "../db/repositories/movie"
 import { IMovie } from "../utils/types"
 
 export const createMovie = async (movie:IMovie) =>{
     return MovieRepository.create(movie, null);
+}
+
+export const findOneMovieById =async (movieId:String) => {
+    return MovieRepository.selectOne({ where: { id: movieId } })
 }
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import * as express from 'express';
 import * as cors from 'cors';
 import bodyParser = require("body-parser");
 import { checkMovie } from './core/schemas/checkMovie';
-import { createMovie } from './core/services/movie';
+import { createMovie, findOneMovieById } from './core/services/movie';
 
 const app: express.Application = express();
 
@@ -21,11 +21,23 @@ app.listen(port, () => console.log(`Listenning on port ${port}`));
 
 app.post('/api/movie/', async (req, res) =>{
     const movie = req.body;
+    console.log('id:', movie.id);
     const response = await checkMovie(movie);
     if (response === null || response === undefined)
         return res.sendStatus(400)
     try{
         return res.status(200).send(await createMovie(response))
+    }catch(err){
+        console.log('ERROR:', err);
+        return res.sendStatus(400)
+    }
+})
+
+app.get('/api/movie/:id', async (req, res) =>{
+    const movieId = req.params.id;
+    console.log('id:', movieId);
+    try{
+        return res.status(200).send(await findOneMovieById(movieId))
     }catch(err){
         console.log('ERROR:', err);
         return res.sendStatus(400)
